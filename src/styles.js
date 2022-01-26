a9c9d617df51033938f2ff6bc2361a870d0e073e
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-26 18:14:31 -0300

    Create global style

---
+++ src/styles.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+export const Styles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    height: 100%;
+    font-family: Roboto, sans-serif;
+    margin: 0px;
+  }
+
+  #root {
+    align-items: center;
+    background-color: #e5e6ed;
+    display: flex;
+    justify-content: center;
+  }
+`;
