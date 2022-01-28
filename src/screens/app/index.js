7b19d886e0a9c596e1a0239a394e0d8a2ea2f63b
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-28 20:22:10 -0300

    Send feedback or request more names

--- src/screens/app/index.js
+++ src/screens/app/index.js
@@ -1,9 +1,12 @@
 import React, { useCallback, useEffect, useState } from "react";
+import { AiOutlineReload } from "react-icons/ai";
+import { BiLoaderAlt } from "react-icons/bi";
+import { BsArrowRightSquareFill } from "react-icons/bs";
 import axios from "axios";
 
 import Search from "./components/search";
 import List from "./components/list";
-import { Container } from "./styles";
+import { Actions, Button, Container } from "./styles";
 
 function App() {
   const [dataset, setDataset] = useState(null);
@@ -36,6 +39,20 @@ function App() {
     }
   }, [dataset, reList]);
 
+  const sendFeedback = useCallback(() => {
+    setLoading("feedback");
+    axios
+      .post(
+        `${process.env.REACT_APP_API_BASE_URL}/datasets/${dataset.id}/feedbacks`,
+        names.filter(({ value }) => value && value !== 0)
+      )
+      .then(() => {
+        setTimeout(() => {
+          setLoading(false);
+        }, 1000);
+      });
+  }, [dataset, names]);
+
   return (
     <div>
       <Search onSelect={setDataset} />
@@ -47,6 +64,32 @@ function App() {
         </p>
 
         <List data={names} onFeedback={(data) => setNames(data)} />
+
+        {names.length > 0 && (
+          <Actions>
+            <Button
+              type="button"
+              onClick={() => reList(dataset)}
+              disabled={loading}
+              $rotate={loading === "list"}
+            >
+              <AiOutlineReload size={20} color="#bbb" />
+              <span>Refresh</span>
+            </Button>
+            <Button
+              onClick={sendFeedback}
+              disabled={loading}
+              $rotate={loading === "feedback"}
+            >
+              <span>Send Feedback</span>
+              {loading === "feedback" ? (
+                <BiLoaderAlt size={20} />
+              ) : (
+                <BsArrowRightSquareFill size={20} color="#bbb" />
+              )}
+            </Button>
+          </Actions>
+        )}
       </Container>
     </div>
   );
--- src/screens/app/styles.js
+++ src/screens/app/styles.js
@@ -16,3 +16,54 @@ export const Container = styled.div`
     }
   }
 `;
+
+export const Actions = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin: 40px 0px 20px;
+  padding-right: 10px;
+`;
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const Button = styled.button`
+  align-items: center;
+  background-color: #ddd;
+  border: 0px;
+  border-radius: 16px;
+  color: #999;
+  cursor: pointer;
+  display: flex;
+  font-family: Roboto, sans-serif;
+  font-size: 14px;
+  height: 39px;
+  margin-right: 10px;
+  opacity: 0.8;
+  padding: 10px 12px;
+
+  > span {
+    margin-right: 8px;
+  }
+
+  svg {
+    margin-right: 5px;
+
+    ${(props) =>
+      props.$rotate &&
+      css`
+        animation: ${rotate} 1s linear infinite;
+      `}
+
+    path {
+      stroke: #666;
+    }
+  }
+`;
