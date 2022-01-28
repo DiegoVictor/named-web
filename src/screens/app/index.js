4a4556044d11663b4d88037f50355e69936525a4
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-28 18:59:46 -0300

    Show generated names

--- src/screens/app/index.js
+++ src/screens/app/index.js
@@ -4,9 +4,39 @@ import Search from "./components/search";
 function App() {
   const [dataset, setDataset] = useState(null);
 
+  const [names, setNames] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const reList = useCallback(() => {
+    setLoading("list");
+
+    axios
+      .get(`${process.env.REACT_APP_API_BASE_URL}/names`, {
+        params: {
+          dataset: dataset.id,
+          tmp: String(dataset.tmp || 0),
+        },
+      })
+      .then((response) => {
+        setNames(
+          response.data.map((name) => ({
+            name,
+          }))
+        );
+        setLoading(false);
+      });
+  }, [dataset]);
   return (
     <div>
       <Search onSelect={setDataset} />
+
+      <Container>
+        <p>
+          Click on the generated names to give feedback, bad feedbacks makes
+          names to not be listed again!
+        </p>
+
+        <List data={names} onFeedback={(data) => setNames(data)} />
+      </Container>
     </div>
   );
 }
